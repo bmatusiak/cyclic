0e0f10b84efdd224c54e825d344eff0730b87f09
bmatusiak <bmatusiak@gmail.com>
2023-08-23 20:44:17 -0400

    update

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hello world");
+const express = require('express')
+const app = express()
+app.all('/', (req, res) => {
+    console.log("Just got a request!")
+    res.send('Yo!')
+})
+app.listen(process.env.PORT || 3000)
