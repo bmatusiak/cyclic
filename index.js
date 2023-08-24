54d7051c8f0c6640f1715ecf839f18d88c9a9062
bmatusiak <bmatusiak@gmail.com>
2023-08-23 20:50:34 -0400

    update

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+
 const app = express()
+
+const server = require('http').createServer(app).listen(8080);
+const gun = GUN({web: server});
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
-app.listen(process.env.PORT || 3000)
